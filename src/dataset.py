a9f9564d0388297515ab66413022633b7ec5aa49
mirqwa <kimrodrikwa@gmail.com>
2025-06-25 19:31:31 +0300

    add directory creation logic

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,22 @@
+import shutil
+from pathlib import Path
+
 import pandas as pd
+
+
+INPUT_DATA_DIR = Path("data")
+DATASETS_DIR = Path("data/dataset_cross_validation")
+TRAIN_IMAGES_DIR = DATASETS_DIR / "images" / "train"
+TRAIN_LABELS_DIR = DATASETS_DIR / "labels" / "train"
+TEST_IMAGES_DIR = DATASETS_DIR / "images" / "test"
+
+
+def create_dataset_directories():
+    for DIR in [TRAIN_IMAGES_DIR, TEST_IMAGES_DIR, DATASETS_DIR]:
+        if DIR.exists():
+            shutil.rmtree(DIR)
+        DIR.mkdir(parents=True, exist_ok=True)
+    shutil.unpack_archive(INPUT_DATA_DIR / "dataset.zip", DATASETS_DIR)
 
 
 def main():
@@ -16,3 +34,7 @@ def main():
     fold_lbl_distrb = get_class_distributions(kfolds, labels_df, cls_idx)
     images, save_path, ds_yamls = create_yml_directories(folds_df, classes)
     copy_validation_data(images, labels, valid_labels_df, save_path)
+
+
+if __name__ == "__main__":
+    main()
